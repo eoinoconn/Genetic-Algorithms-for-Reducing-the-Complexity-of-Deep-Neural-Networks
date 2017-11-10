432fbe61d57cb02b1353cb6d67e7863e9d78e768
eoinoconn <eoinoconn@gmail.com>
2017-11-10 17:04:42 +0000

    sorry attempt at new CNN

--- Python/Basic_CNN/notMNist.py
+++ Python/Basic_CNN/notMNist.py
@@ -2,11 +2,14 @@
 # before proceeding further.
 from __future__ import print_function
 import numpy as np
-import tensorflow as tf
 from six.moves import cPickle as pickle
-from six.moves import range
+from keras.utils import plot_model
+import keras as keras
+from keras import backend as K
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Dropout
+from keras.losses import sparse_categorical_crossentropy
+from keras.layers import Dense, Activation, Conv2D, Conv1D, MaxPooling1D, Dropout, Flatten
+from keras.optimizers import Adadelta
 
 pickle_file = 'notMNIST.pickle'
 
@@ -28,31 +31,65 @@ with open(pickle_file, 'rb') as f:
 
 
 def reformat(dataset, labels):
-    dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+    dataset = np.expand_dims(dataset.reshape((-1, image_size * image_size)).astype(np.float32), axis=2)
     # Map 0 to [1.0, 0.0, 0.0 ...], 1 to [0.0, 1.0, 0.0 ...]
-    labels = (np.arange(num_labels) == labels[:, None]).astype(np.float32)
+    labels = np.expand_dims((np.arange(num_labels) == labels[:, None]).astype(np.float32), axis=2)
     return dataset, labels
 
+if K.image_data_format() == 'channels_first':
+    train_dataset = train_dataset.reshape(train_dataset.shape[0], 1, image_size, image_size)
+    test_dataset = test_dataset.reshape(test_dataset.shape[0], 1, image_size, image_size)
+    input_shape = (1, image_size, image_size)
+else:
+    train_dataset = train_dataset.reshape(train_dataset.shape[0], image_size, image_size, 1)
+    test_dataset = test_dataset.reshape(test_dataset.shape[0], image_size, image_size, 1)
+    input_shape = (image_size, image_size, 1)
 
-train_dataset, train_labels = reformat(train_dataset, train_labels)
-valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
-test_dataset, test_labels = reformat(test_dataset, test_labels)
+# train_dataset, train_labels = reformat(train_dataset, train_labels)
+# valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+# test_dataset, test_labels = reformat(test_dataset, test_labels)
 print('Training set', train_dataset.shape, train_labels.shape)
 print('Validation set', valid_dataset.shape, valid_labels.shape)
 print('Test set', test_dataset.shape, test_labels.shape)
 
+num_classes = 10
+
+y_train = keras.utils.to_categorical(train_labels, num_classes)
+y_test = keras.utils.to_categorical(test_labels, num_classes)
 
 model = Sequential()
 
-model.add(Conv2D(64, (2, 2), activation='relu', input_shape=(784, 4)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.summary()
-model.add(MaxPooling2D(Conv2D(32, [2, 2], activation='softmax')))
-model.summary()
-model.add(Dense(units=10))
-model.summary()
-model.add(Dropout(0.1))
+#model.add(Conv1D(64, 2, activation='relu', input_shape = (784, 1)))
+#model.add(MaxPooling1D(pool_size=2))
+#model.add(Conv1D(32, 2, activation='relu'))
+#model.add(Dense(units=10, activation='relu'))
+#model.add(Dropout(0.1))
+#model.add(Flatten())
+#model.add(Dense(units=10, activation='softmax'))
 
-from keras.utils import plot_model
-plot_model(model, to_file='model.png')
+model.add(Dense(10, input_shape=(28, 28, 1)))
+# model.add(Dense(10))
 
+
+if False:
+    model.add(Conv1D(32, kernel_size=3,
+                     activation='relu',
+                     input_shape=(784,1)))
+    model.add(Conv1D(64, 3, activation='relu'))
+    model.add(MaxPooling1D(pool_size=(2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+    model.summary()
+    model.compile(loss=sparse_categorical_crossentropy,
+                  optimizer=Adadelta(),
+                  metrics=['accuracy'])
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+model.fit(train_dataset, train_labels, epochs=2, batch_size=128)
+loss_and_metrics = model.evaluate(train_dataset, train_labels, batch_size=512)
