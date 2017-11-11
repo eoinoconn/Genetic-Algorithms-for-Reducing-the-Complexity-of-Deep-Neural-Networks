80e5b0d7a11dc8353e0233900f5c131ea3418bc1
Unknown <eoinoconn@gmail.com>
2017-11-11 17:07:58 +0000

    added two CNNs and a python file to retrieve the dataset

--- Python/Basic_CNN/simple_notMNIST_CNN.py
+++ Python/Basic_CNN/simple_notMNIST_CNN.py
@@ -7,7 +7,7 @@ import keras as keras
 from keras import backend as K
 from keras.models import Sequential
 from keras.losses import sparse_categorical_crossentropy
-from keras.layers import Dense, Activation, Conv2D, Conv1D, MaxPooling2D, Dropout, Flatten, AveragePooling2D
+from keras.layers import Dense, Activation, Conv2D, Conv1D, MaxPooling1D, Dropout, Flatten
 from keras.optimizers import Adadelta
 
 pickle_file = 'notMNIST.pickle'
@@ -25,8 +25,8 @@ with open(pickle_file, 'rb') as f:
     print('Validation set', valid_dataset.shape, valid_labels.shape)
     print('Test set', test_dataset.shape, test_labels.shape)
 
-    image_size = 28
-    num_labels = 10
+image_size = 28
+num_labels = 10
 
 
 def reformat(dataset):
@@ -38,34 +38,25 @@ train_dataset = reformat(train_dataset)
 valid_dataset = reformat(valid_dataset)
 test_dataset = reformat(test_dataset)
 
-
-num_classes = 10
-
-y_train = keras.utils.to_categorical(train_labels, num_classes)
-y_test = keras.utils.to_categorical(test_labels, num_classes)
+train_labels = keras.utils.to_categorical(train_labels, num_labels)
+valid_labels = keras.utils.to_categorical(valid_labels, num_labels)
+test_labels = keras.utils.to_categorical(test_labels, num_labels)
 
 model = Sequential()
+
 model.add(Conv2D(32, (2, 2), input_shape=(28, 28, 1), padding='same', activation='relu'))
-model.add(AveragePooling2D(pool_size=4))
-model.add(Conv2D(64, (2, 2), padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
-model.summary()
 
-if False:
-    print('Training set', train_dataset.shape, y_train.shape)
-    print('Validation set', valid_dataset.shape, valid_labels.shape)
-    print('Test set', test_dataset.shape, y_test.shape)
+print('Training set', train_dataset.shape, train_labels.shape)
+print('Validation set', valid_dataset.shape, valid_labels.shape)
+print('Test set', test_dataset.shape, test_labels.shape)
 
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='sgd',
-                  metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',
+                optimizer='sgd',
+                metrics=['accuracy'])
 
-    model.fit(train_dataset, y_train, epochs=5, batch_size=100)
-    loss_and_metrics = model.evaluate(test_dataset, y_test, batch_size=100)
-    print('Test loss:', loss_and_metrics[0])
-    print('Test accuracy:', loss_and_metrics[1])
+model.fit(train_dataset, train_labels, epochs=5, batch_size=100, validation_data=(valid_dataset, valid_labels))
+loss_and_metrics = model.evaluate(test_dataset, test_labels, batch_size=100)
+print('\nTest loss:', loss_and_metrics[0])
+print('Test accuracy:', loss_and_metrics[1])
