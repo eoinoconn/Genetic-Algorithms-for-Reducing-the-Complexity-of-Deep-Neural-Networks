ec699ee5b6d5532bfc92e8da1f1f16c8318adf26
13335561 <eoinoconn@gmail.com>
2018-01-06 13:45:26 +0000

    Added support for paretn object with convolutional layer

--- Python/Encoding/mutate.py
+++ Python/Encoding/mutate.py
@@ -1,4 +1,4 @@
-from Python.Encoding.chromosome import Chromosome, LAYER_DEPTH
+from Python.Encoding.chromosome import Chromosome, LAYER_DEPTH, INPUT_SHAPE
 import random
 
 
@@ -39,7 +39,27 @@ def create_parent():
     chromosome.add_layer(convolutional_layer())
     return chromosome
 
-def convolutional_layer():
+
+def convolutional_layer(input = False):
     layer = [0 for x in range(LAYER_DEPTH)]
-    layer[0] = 1
-    
+    layer[0] = 2    # Sets convolutional layer
+    layer[1] = random.randrange(1, 16) * 16 # sets layer units
+    if input:
+        layer[2] = 1    # Sets input layer
+    else:
+        layer[2] = 0    # Sets hidden layer
+    layer[3] = random.randrange(1, 5)   # Sets slide size
+    layer[4] = set_activation()
+
+
+def set_activation():
+    ran_active = random.randrange(0, 3)
+    if ran_active == 0:
+        return 'relu'
+    elif ran_active == 1:
+        return 'sigmoid'
+    elif ran_active == 2:
+        return 'linear'
+    else:
+        return 'elu'
+
