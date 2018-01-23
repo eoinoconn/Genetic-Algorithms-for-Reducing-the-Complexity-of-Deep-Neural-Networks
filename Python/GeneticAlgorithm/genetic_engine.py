458bbdbe328ec85eb61f40143ae1a462a6b767d2
Eoin <eoinoconn@gmail.com>
2018-01-23 15:42:52 +0000

    changed test file
    made it cleaner, got rid of useless code

--- Python/GeneticAlgorithm/genetic_engine.py
+++ Python/GeneticAlgorithm/genetic_engine.py
@@ -1,20 +1,27 @@
+from keras.utils import to_categorical
 from Python.GeneticAlgorithm.mutate import create_parent, mutate
+from keras.datasets import mnist
 import operator
 import logging
+import numpy as np
 
 POOL_SIZE = 6
+IMAGE_SIZE = 28
+NUM_LABELS = 10
 
 
-def get_best(max_generations):
+def get_best(max_generations, fn_unpack_training_data):
 
     logger = logging.getLogger('geneticEngine')
     logger.info('Starting genetic engine...')
+
+    training_data = fn_unpack_training_data()
 
     generation = 1
     population = create_population(logger)
     logger.info("Generation number: %d", generation)
     while generation < max_generations:
-        assess_population_fitness(population, logger)
+        assess_population_fitness(population, training_data, logger)
         mutate_population(population, logger)
         generation += 1
 
@@ -27,11 +34,11 @@ def create_population(logger):
     return pool
 
 
-def assess_population_fitness(population, logger):
+def assess_population_fitness(population, training_data, logger):
     i = 0
     for chromosome in population:
         logger.info("getting fitness of chromosome %d", i+1)
-        chromosome.get_fitness()
+        chromosome.assess_fitness(training_data)
         i += 1
     population.sort(key=operator.attrgetter('fitness'))
 
@@ -40,5 +47,10 @@ def mutate_population(population, logger):
     i = 0
     for chromosome in population:
         logger.info("mutating chromosome %d", i+1)
-        mutate(chromosome.genes)
+        mutate(chromosome)
         i += 1
+
+
+
+
+
