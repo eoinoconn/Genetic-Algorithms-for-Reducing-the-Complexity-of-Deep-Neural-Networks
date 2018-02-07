18e8063c8d5a3b7bb3e592dde59dc721d3fc9aa3
Eoin <eoinoconn@gmail.com>
2018-02-07 11:33:22 +0000

    added CIFAR10 test file

---
+++ Python/GeneticAlgorithm/CIFAR10_test.py
@@ -0,0 +1,61 @@
+"""
+In this file we will test the encoding and train a basic network using the built structure
+
+"""
+from __future__ import print_function
+import time
+
+from Python.GeneticAlgorithm.fitness import assess_chromosome_fitness, evaluate_best_chromosome
+from Python.GeneticAlgorithm.genetic_engine import *
+
+
+import unittest
+import logging
+from logging import config
+
+num_labels = 10
+
+
+def unpack_training_data():
+    (train_dataset, train_labels), (test_dataset, test_labels) = cifar10.load_data()
+
+    train_labels = to_categorical(train_labels, NUM_LABELS)
+    test_labels = to_categorical(test_labels, NUM_LABELS)
+
+    return {"train_dataset": train_dataset, "train_labels": train_labels,
+            "valid_dataset": None, "valid_labels": None}
+
+
+def unpack_testing_data():
+    (train_dataset, train_labels), (test_dataset, test_labels) = cifar10.load_data()
+
+    print('x_train shape:', train_dataset.shape)
+    print(train_dataset.shape[0], 'train samples')
+    print(test_dataset.shape[0], 'test samples')
+
+    train_labels = to_categorical(train_labels, NUM_LABELS)
+    test_labels = to_categorical(test_labels, NUM_LABELS)
+
+    return {"train_dataset": train_dataset, "train_labels": train_labels,
+            "valid_dataset": None, "valid_labels": None,
+            "test_dataset": test_dataset, "test_labels": test_labels}
+
+
+class MNISTTest(unittest.TestCase):
+
+    def test_encoding(self):
+        logging.config.fileConfig('logs/logging.conf')
+        logger = logging.getLogger('testFile')
+
+        logger.info("starting test...")
+
+        start = time.time()
+        best = get_best(100, (32, 32, 3), unpack_training_data)
+        end = time.time()
+
+        logger.info("time to best %f", end-start)
+        self.assertTrue(evaluate_best_chromosome(best, **unpack_testing_data()) > 0.99)
+
+
+if __name__ == '__main__':
+    unittest.main()
