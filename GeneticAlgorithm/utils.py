9c158c8240eb442887e95291401aa421efdac571
Eoin <eoinoconn@gmail.com>
2018-03-26 17:07:57 +0100

    improved logging

--- GeneticAlgorithm/utils.py
+++ GeneticAlgorithm/utils.py
@@ -28,16 +28,19 @@ def check_valid_geneset(genes, logger=logging.getLogger(__name__)):
     """
 
     current_dimension = genes.input_shape[0]
-    logger.info("checking for valid geneset; conv dimensions %d", current_dimension)
+    logger.info("checking for valid geneset for ; conv dimensions %d", current_dimension)
     for layer in genes.iterate_layers():
         if layer[0] == 2:
             if layer[7] == 'same':
                 current_dimension = conv_layer_output_size(current_dimension, 0, layer[2], 0)
+                logger.info("Check valid geneset: dimension %d", current_dimension)
             else:
                 padding = 0
                 current_dimension = conv_layer_output_size(current_dimension, layer[3], layer[2], padding)
+                logger.info("Check valid geneset: dimension %d", current_dimension)
             if layer[5] is not 0:
                 current_dimension = pooling_layer_output_size(current_dimension, layer[6], layer[8])
+                logger.info("Check valid geneset: dimension %d", current_dimension)
         elif layer[0] == 3:
             break
     if current_dimension < 1:
