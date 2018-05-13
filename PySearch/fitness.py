6de65361f0546fc9f964e6201f86c148440ea190
13335561 <eoinoconn@gmail.com>
2018-05-13 13:48:25 +0100

    Refactor: improved logging of fitness object

--- PySearch/fitness.py
+++ PySearch/fitness.py
@@ -6,7 +6,12 @@ from keras.callbacks import EarlyStopping, History, TensorBoard
 from keras.utils import print_summary
 
 class Fitness(object):
-
+    """
+    Fitness object to evaluate chromosome.
+    
+    Arguments:
+        efficiency_balance -- scaling factor for fitness function (Default = 0.0000001).
+    """
     def __init__(self, efficiency_balance=0.000001):
         self.efficiency_balance = efficiency_balance
         self.epochs = 0
@@ -26,7 +31,11 @@ class Fitness(object):
         self._initialise_values()
 
     def _initialise_values(self):
-
+        """
+        Initialise training parameters.
+        Overwrites chromosome hyperparameters if 
+        training.parameters.overwrite_hyperparameters = True
+        """
         if self.config['training.parameters'].getboolean('overwrite_hyperparameters'):
             self.logger_fitness.info("Overwriting hyperparameters")
             self.efficiency_balance = float(self.config['training.parameters']['efficiency_balance'])
@@ -44,17 +53,35 @@ class Fitness(object):
         self.verbose = int(self.config['training.parameters']['fit_verbose'])
 
     def __call__(self, model, hyper_params,
-                                  efficiency_balance=0.0000001,
                                   train_dataset=None, train_labels=None,
                                   valid_dataset=None, valid_labels=None,
                                   test_dataset=None, test_labels=None,
-                                  evaluate_best=False, log_csv=False,
                                   eval_epochs=None):
+        """
+        Trains model and evaulates using validation data or test data.
+        
+        Arguments:
+        
+            model -- the keras model of the chromosome.
+            hyper_params -- list of chromosome model hyper-parameters.
+            train_dataset -- numpy array of trainning data.
+            train_labels -- numpy array of trainning data labels.
+            valid_dataset --  numpy array of validation data.
+            valid_labels -- numpy array of validation data labels.
+            test_dataset --  numpy array of testing data.
+            test_labels -- numpy array of testing data.
+            evaluate_best -- if true, evaluates on test data (Default=False).
+            eval_epochs -- if not None, overwrites number of training epochs (Default=None).
+        
+        Returns:
 
+            Fitness -- [0,1] Fitness of the evaluated chromosome. If efficient.cost.function is False
+                Fitness is assigned the value of accuracy.
+            Accuracy -- [0,1] Accuracy of evaluated network.
+            Parameters -- [0, inf] Number of trainable parameters a network has.
+        """
         # log geneset model
         print_summary(model, print_fn=self.logger_fitness.info)
-
-        self.logger_fitness.info("Model built successfully")
 
         if eval_epochs is not None:
             self.epochs = eval_epochs
@@ -65,7 +92,6 @@ class Fitness(object):
 
         self.logger_fitness.info("Model compiled succesfully, beginning training...")
 
-        # get callbacks
         callbacks = self._get_callbacks()
 
         if valid_dataset is None:
@@ -84,28 +110,22 @@ class Fitness(object):
                              callbacks=callbacks,
                              verbose=self.verbose)
 
-        self.logger_fitness.info("Model trained successfully,  saving weights...")
+        self.logger_fitness.info("Model trained successfully")
 
-        # store num of model parameters
         parameters = model.count_params()
         accuracy = hist.history['val_acc'][-1]
 
-        self.logger_fitness.info("Weights saved")
-
-        if evaluate_best:
+        # if test dataset present evalualte using test data.
+        if test_dataset is not None:
             loss_and_metrics = model.evaluate(test_dataset, test_labels,
                                               batch_size=self.batch_size,
                                               verbose=self.verbose)
-
-            if log_csv:
-                with open('GeneticAlgorithm/logs/validvtest.csv', 'a', newline='') as csvfile:
-                    spamwriter = csv.writer(csvfile, delimiter=' ',
-                                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                    spamwriter.writerow([accuracy, ',', loss_and_metrics[1]])
             accuracy = loss_and_metrics[1]
 
+        # if fitness function is active utilise.
         if self.config['efficient.cost.function'].getboolean('enable_efficiency_function'):
             fitness = self.cost_function(accuracy, self.efficiency_balance, parameters)
+        # else assign accuracy as fitness
         else:
             self.logger_fitness.info("Fitness function disabled")
             fitness = accuracy
@@ -118,10 +138,20 @@ class Fitness(object):
 
     @staticmethod
     def cost_function(accuracy, efficiency_balance, parameters):
+        """
+        Returns result of fitness function.
+        :return: float
+        """
         return accuracy - (efficiency_balance * parameters)
 
 
     def _get_callbacks(self):
+        """
+        Initilises appropriate callbacks for training.
+
+        Callbacks include Early stopping and TensorBoard, with parameters set by training_parameters.ini
+        :return: List of callback objects
+        """
         callbacks = []
         if self.config['early.stopping'].getboolean('early_stopping'):
             callbacks.append(EarlyStopping(monitor=self.config['early.stopping']['monitor'],
